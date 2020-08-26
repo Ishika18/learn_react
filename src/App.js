f40e16b4380befe893ce7fca05084bbd081ecf72
Shagun <shagunphw@gmail.com>
2020-08-26 12:24:51 -0700

    router set up

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
 import './App.css';
 import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{ height: '300px', position: 'relative' }}>
+    <div className='demo-big-content'>
       <Layout fixedHeader>
-        <Header title={<span><span style={{ color: '/ddd' }}>Area / </span><strong>The Title</strong></span>}>
+        <Header title={<span><span style={{ color: '/ddd' }}></span><strong>Title</strong></span>}>
           <Navigation>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
+            <Link to="/aboutme">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/resume">Resume</Link>
           </Navigation>
         </Header>
         <Drawer title="Title">
           <Navigation>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
+            <Link to="/aboutme">About</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/resume">Resume</Link>
           </Navigation>
         </Drawer>
-        <Content />
+        <Content>
+          <div className='page-content' />
+          <Main />
+        </Content>
       </Layout>
     </div>
   );
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-class Content extends Component {
+class Contact extends Component {
     render() {
         return (
             <div>
-                <h1>Content Page</h1>
+                <h1>Contact Page</h1>
             </div>
         )
     }
 }
 
-export default Content;
+export default Contact;
