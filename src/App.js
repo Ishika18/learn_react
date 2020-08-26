14ff7572e39ed5d4a2ba634ff46029ced4a30ef1
Shagun <shagunphw@gmail.com>
2020-08-26 13:53:42 -0700

    mmaterial-ui and app navbar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
 import React from 'react';
-import './App.css';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
-import Main from './components/main';
+import './App.css'; 
+import Home from './components/index';
 import { Link } from 'react-router-dom';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className='demo-big-content'>
-      <Layout fixedHeader>
-        <Header title={<span><span style={{ color: '/ddd' }}></span><strong>Title</strong></span>}>
-          <Navigation>
-            <Link to="/aboutme">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/resume">Resume</Link>
-          </Navigation>
-        </Header>
-        <Drawer title="Title">
-          <Navigation>
-            <Link to="/aboutme">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/resume">Resume</Link>
-          </Navigation>
-        </Drawer>
-        <Content>
-          <div className='page-content' />
-          <Main />
-        </Content>
-      </Layout>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
